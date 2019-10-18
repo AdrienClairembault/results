347fefb12aea655411c7e2b23473f19a6aab6d05
Adrien Clairembault <aclairembault@teclib.com>
2019-10-18 11:19:04 +0200

    Results

---
+++ results/sample-module/fr.dict.sample-module.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Localized data
+ *
+ * @copyright   Copyright (C) 2013 XXXXX
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+Dict::Add('FR FR', 'French', 'Français', array(
+	// Dictionary entries go here
+	'Menu:my_menu' => "Mon menu",
+	'Class:MyObject' => "Mon objet",
+	'Class:MyObject/Attribute:name' => "Nom",
+	'Class:MyObject/Attribute:email' => "Email",
+	'Class:MyObject/Attribute:description' => "Description",
+	'Class:MyObject/Attribute:phone' => "Téléphone",
+	'Class:MyObject/Attribute:status' => "Statut",
+	'Class:MyObject/Attribute:status/Value:new' => "Nouveau",
+	'Class:MyObject/Attribute:status/Value:validated' => "Validé",
+	'Class:MyObject/Attribute:status/Value:closed' => "Clos",
+	'Class:MyObject/Attribute:my_enum' => "Énumération",
+	'Class:MyObject/Attribute:my_enum/Value:a' => "A",
+	'Class:MyObject/Attribute:my_enum/Value:b' => "B",
+	'Class:MyObject/Attribute:my_enum/Value:c' => "C",
+	'Class:MyObject/Attribute:my_enum/Value:d' => "D",
+	'Class:MyObject/Attribute:my_tag' => "Tag",
+	'Class:MyObject/Attribute:product_id' => "Produit",
+	'Class:MyObject/Attribute:product_status' => "Statut du produit",
+	'Class:MyObject/Attribute:pc_list' => "PC(s)",
+	'Class:MyObject/Attribute:server_list' => "Serveur(s)",
+	'Class:MyObject/Attribute:close_date' => "Date de clôture",
+	'Class:MyObject/Attribute:last_update' => "Date de dernière mise à jour",
+	'Class:MyObject/Attribute:my_int' => "Entier",
+	'Class:MyObject/Attribute:nb_modification_description' => "Nombre de modifications de la description",
+	'Class:MyObject/Stimulus:ev_validated' => "Valider",
+	'Class:MyObject/Stimulus:ev_closed' => "Fermer",
+));
+?>
